d6a7c117dba37fa2ea3c5718e22b49544283db89
SianDurginAtLREI <149105794+SianDurginAtLREI@users.noreply.github.com>
2023-11-09 14:55:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var name = "Jacob";
+
+setInterval(() => {
 var today = moment().format('dddd, MMMM D, YYYY');
-var time = moment().format('h:mm A');
+var time = moment().format('h:mm:ss A');
 document.getElementById("app").innerHTML = `My name is ${name}<br><br>Today is ${today}<br>Current time: ${time}`;
+}, 1000);
