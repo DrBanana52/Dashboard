bff9cf9ea9fa4059a32f6a53a4b45d30a588ce44
Jacob Farkas <jfarkas@lrei.org>
2023-11-09 14:04:59 -0500

    Update script.js
    Added today and time variables, using the moment.js library and format

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var name = "Jacob"
-document.getElementById("app").innerHTML = `My name is ${name}`;
+var name = "Jacob";
+var today = moment().format('MMMM Do YYYY');
+var time = moment().format('h:mm A');
+document.getElementById("app").innerHTML = `My name is ${name}<br><br>Today is ${today}<br>Current time: ${time}`;
