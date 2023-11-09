3e8d1960a2afb550b40128fdf2916554f416e0d9
Jacob Farkas <jfarkas@lrei.org>
2023-11-09 14:09:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var name = "Jacob";
-var today = moment().format('MMMM Do YYYY');
+var today = moment().format('dddd, MMMM D, YYYY');
 var time = moment().format('h:mm A');
 document.getElementById("app").innerHTML = `My name is ${name}<br><br>Today is ${today}<br>Current time: ${time}`;
